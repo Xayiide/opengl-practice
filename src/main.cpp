557a7d7e62fe1d103090b4635cca598498cd2542
Xayiide <wickedcheerio@protonmail.com>
2023-09-28 23:16:48 +0200

    Tutorial 2

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,27 @@
 
 using namespace glm; /* en lugar de glm3::vec3, sólo vec3 */
 
+#include "shader.hpp"
+
 int main()
 {
     GLFWwindow *window;
+    GLuint      VertexArrayID;
+    GLuint      vertexbuffer;
+    GLuint      programID;
+
+    static const GLfloat g_vertex_buffer_data[] = {
+        -1.0f, -1.0f, 0.0f, /* */
+         1.0f, -1.0f, 0.0f, /* */
+         0.0f,  1.0f, 0.0f, /* */
+    };
 
     if (glfwInit() == GL_FALSE) {
         fprintf(stderr, "Error al inicializar GLFW.\n");
         exit(EXIT_FAILURE);
     }
+
+    
 
     glfwWindowHint(GLFW_SAMPLES, 4); /* 4x antialiasing */
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); /* Queremos OpenGL 3.3 */
@@ -31,25 +44,72 @@ int main()
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
-
     glfwMakeContextCurrent(window); /* Inicializa GLEW */
-    glewExperimental = true; /* Necesario para perfil core */
 
+    /* Inicializa GLEW */
+    glewExperimental = true; /* Necesario para perfil core */
     if (glewInit() != GLEW_OK) {
         fprintf(stderr, "Error al inicializar GLEW.\n");
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
 
+    /* Para poder capturar la pulsación de teclas */
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
+    /* Color negro de fondo */
+    glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+    /* Crea Vertex Array Object (VAO) y ponerlo como primero */
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
+
+    programID = LoadShaders("vertexShader.glsl",
+                            "fragmentShader.glsl");
+
+    /* "Damos" el triángulo a OpenGL */
+    /* Genera 1 buffer y pone el identificador en vertexbuffer */
+    glGenBuffers(1, &vertexbuffer);
+    /* Haz que los siguientes comandos se dirigan a vertexbuffer */
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    /* Da nuestros vértices a OpenGL */
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data),
+            g_vertex_buffer_data, GL_STATIC_DRAW);
+
+
     do {
-        glClear(GL_COLOR_BUFFER_BIT); /* Limpia la pantalla */
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); /* Limpia la pantalla */
+
+        /* Usa nuestro shader */
+        glUseProgram(programID);
+
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(
+            0,         /* Tiene que ser igual al layout del shader */
+            3,         /* Tamaño                                   */
+            GL_FLOAT,  /* Tipo                                     */
+            GL_FALSE,  /* ¿normalizado?                            */
+            0,         /* Stride                                   */
+            (void *) 0 /* Desplazamiento del array buffer          */
+        );
+
+        /* Dibuja el triángulo */
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glDisableVertexAttribArray(0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
-    } while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &
-             glfwWindowShouldClose(window) == 0);
+    } while ((glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS ) &&
+             (glfwWindowShouldClose(window) == 0));
 
+    glDeleteBuffers(1, &vertexbuffer);
+    glDeleteVertexArrays(1, &VertexArrayID);
+    glDeleteProgram(programID);
+
+
+    /* Cierra OpenGL */
+    glfwTerminate();
 }
 
---
+++ src/shader.hpp
@@ -0,0 +1,7 @@
+#ifndef _SHADER_HPP_
+#define _SHADER_HPP_
+
+GLuint LoadShaders(const char *vertex_file_path,
+                   const char *fragment_file_path);
+
+#endif
