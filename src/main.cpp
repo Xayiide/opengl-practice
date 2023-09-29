1531abd520013aae6d567ced5b1f65a318d3baf9
Xayiide <wickedcheerio@protonmail.com>
2023-09-29 15:10:23 +0200

    Tutorial 4: El cubo cambia cada segundo de color

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> /* rand */
+#include <time.h>   /* time */
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -8,6 +9,8 @@
 #include <glm/glm.hpp>                  /* mates 3D */
 
 #include "shader.hpp"
+
+static void randomize_color(GLfloat buffer[]);
 
 int main()
 {
@@ -21,6 +24,9 @@ int main()
     glm::mat4   View;
     glm::mat4   Model;
     glm::mat4   MVP;
+    int         frames = 1;
+
+    srand((unsigned int) time(NULL));
 
     static const GLfloat g_vertex_buffer_data[] = {
         -1.0f, -1.0f, -1.0f, // triangle 1 : begin
@@ -61,44 +67,8 @@ int main()
          1.0f, -1.0f,  1.0f
     };
 
-    static const GLfloat g_color_buffer_data[] = {
-        0.583f,  0.771f,  0.014f,
-        0.609f,  0.115f,  0.436f,
-        0.327f,  0.483f,  0.844f,
-        0.822f,  0.569f,  0.201f,
-        0.435f,  0.602f,  0.223f,
-        0.310f,  0.747f,  0.185f,
-        0.597f,  0.770f,  0.761f,
-        0.559f,  0.436f,  0.730f,
-        0.359f,  0.583f,  0.152f,
-        0.483f,  0.596f,  0.789f,
-        0.559f,  0.861f,  0.639f,
-        0.195f,  0.548f,  0.859f,
-        0.014f,  0.184f,  0.576f,
-        0.771f,  0.328f,  0.970f,
-        0.406f,  0.615f,  0.116f,
-        0.676f,  0.977f,  0.133f,
-        0.971f,  0.572f,  0.833f,
-        0.140f,  0.616f,  0.489f,
-        0.997f,  0.513f,  0.064f,
-        0.945f,  0.719f,  0.592f,
-        0.543f,  0.021f,  0.978f,
-        0.279f,  0.317f,  0.505f,
-        0.167f,  0.620f,  0.077f,
-        0.347f,  0.857f,  0.137f,
-        0.055f,  0.953f,  0.042f,
-        0.714f,  0.505f,  0.345f,
-        0.783f,  0.290f,  0.734f,
-        0.722f,  0.645f,  0.174f,
-        0.302f,  0.455f,  0.848f,
-        0.225f,  0.587f,  0.040f,
-        0.517f,  0.713f,  0.338f,
-        0.053f,  0.959f,  0.120f,
-        0.393f,  0.621f,  0.362f,
-        0.673f,  0.211f,  0.457f,
-        0.820f,  0.883f,  0.371f,
-        0.982f,  0.099f,  0.879f
-    };
+    static GLfloat g_color_buffer_data[12 * 3 * 4];
+    randomize_color(g_color_buffer_data);
 
     if (glfwInit() == GL_FALSE) {
         fprintf(stderr, "Error al inicializar GLFW.\n");
@@ -227,6 +197,17 @@ int main()
 
         glDisableVertexAttribArray(0);
 
+
+        frames++;
+        if (frames % 24 == 0) {
+            randomize_color(g_color_buffer_data);
+            glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data),
+                    g_color_buffer_data, GL_STATIC_DRAW);
+            frames = 0;
+        }
+
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     } while ((glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS ) &&
@@ -241,3 +222,14 @@ int main()
     glfwTerminate();
 }
 
+
+static void randomize_color(GLfloat buffer_data[])
+{
+    int i;
+
+    for (i = 0; i < 12 * 3; i++) {
+        buffer_data[3 * i + 0] = (float) (rand() / (float) RAND_MAX);
+        buffer_data[3 * i + 1] = (float) (rand() / (float) RAND_MAX);
+        buffer_data[3 * i + 2] = (float) (rand() / (float) RAND_MAX);
+    }
+}
